76f88ddcc217f26ca757718888388fd4771b2e0c
hampusolin <hampusolin@outlook.com>
2018-04-17 13:55:29 +0200

    Assembly batch script ready to run

--- code/assemble.sh
+++ code/assemble.sh
@@ -10,5 +10,4 @@
 module load bioinfo-tools
 module load megahit/1.1.2
 # Your commands
-megahit --kmin-1pass /home/haol7814/ga2018/data/trimmed_data/SRR4342129_1.paired.trimmed.fastq.gz /home/haol7814/ga2018/data/trimmed_data/SRR4342129_2.paired.trimmed.fastq.gz /home/haol7814/ga2018/data/trimmed_data/SRR4342133_1.paired.trimmed.fastq.gz  /home/haol7814/ga2018/data/trimmed_data/SRR4342133_2.paired.trimmed.fastq.gz -o /home/haol7814/ga2018/analyses/02_assembly/genome_assembly.asm
-
+megahit --kmin-1pass -1 /home/haol7814/ga2018/data/trimmed_data/SRR4342129_1.paired.trimmed.fastq.gz,/home/haol7814/ga2018/data/trimmed_data/SRR4342133_1.paired.trimmed.fastq.gz -2 /home/haol7814/ga2018/data/trimmed_data/SRR4342129_2.paired.trimmed.fastq.gz,/home/haol7814/ga2018/data/trimmed_data/SRR4342133_2.paired.trimmed.fastq.gz -o /home/haol7814/ga2018/analyses/02_assembly
