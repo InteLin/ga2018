9d08e4af0f41326161cb2d0b2a1f2514d5a9d68a
hampusolin <hampusolin@outlook.com>
2018-04-17 14:46:51 +0200

    Started DNA mapping script

---
+++ code/map_DNA.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH -A g2018003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 07:00:00
+#SBATCH -J mapping_DNA
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user hampus.olin.7814@student.uu.se
+# Load modules
+module load 
+# Your commands
+
+
