5d5cd59c4706162c2900cbd1057e3f184321e96e
Hampus Olin <haol7814@rackham4.uppmax.uu.se>
2018-04-25 12:43:13 +0200

    Updated scripts

--- code/assemble.sh
+++ code/assemble.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2018003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 08:00:00
+#SBATCH -t 11:00:00
 #SBATCH -J genome_assembly
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user hampus.olin.7814@student.uu.se
@@ -10,4 +10,4 @@
 module load bioinfo-tools
 module load megahit/1.1.2
 # Your commands
-megahit --kmin-1pass -1 /home/haol7814/ga2018/data/trimmed_data/SRR4342129_1.paired.trimmed.fastq.gz,/home/haol7814/ga2018/data/trimmed_data/SRR4342133_1.paired.trimmed.fastq.gz -2 /home/haol7814/ga2018/data/trimmed_data/SRR4342129_2.paired.trimmed.fastq.gz,/home/haol7814/ga2018/data/trimmed_data/SRR4342133_2.paired.trimmed.fastq.gz -o /home/haol7814/ga2018/analyses/02_assembly
+megahit --kmin-1pass -t 2 --k-list 59,79,99 -1 /home/haol7814/ga2018/data/trimmed_data/SRR4342129_1.paired.trimmed.fastq.gz,/home/haol7814/ga2018/data/trimmed_data/SRR4342133_1.paired.trimmed.fastq.gz -2 /home/haol7814/ga2018/data/trimmed_data/SRR4342129_2.paired.trimmed.fastq.gz,/home/haol7814/ga2018/data/trimmed_data/SRR4342133_2.paired.trimmed.fastq.gz -o /home/haol7814/ga2018/analyses/02_assembly
--- code/map_DNA.sh
+++ code/map_DNA.sh
@@ -7,7 +7,9 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user hampus.olin.7814@student.uu.se
 # Load modules
-module load 
+module load bioinfo-tools
+module load bwa/0.7.17
 # Your commands
-
+bwa index /home/haol7814/ga2018/analyses/02_assembly/final.contigs.fa
+bwa mem /home/haol7814/ga2018/analyses/02_assembly/final.contigs.fa /home/haol7814/ga2018/data/trimmed_data/SRR4342129_1.paired.trimmed.fastq.gz /home/haol7814/ga2018/data/trimmed_data/SRR4342133_1.paired.trimmed.fastq.gz /home/haol7814/ga2018/data/trimmed_data/SRR4342129_2.paired.trimmed.fastq.gz /home/haol7814/ga2018/data/trimmed_data/SRR4342133_2.paired.trimmed.fastq.gz
 
