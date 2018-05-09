041266cc680f69cb296b5dc8c73cb1e45c50e8b7
Hampus Olin <haol7814@rackham3.uppmax.uu.se>
2018-05-09 14:44:32 +0200

    Piping through samtools to get bam files

--- code/map_DNA_SRR4342129.sh
+++ code/map_DNA_SRR4342129.sh
@@ -11,5 +11,5 @@ module load bioinfo-tools
 module load bwa/0.7.17
 # Your commands
 bwa index /home/haol7814/ga2018/analyses/02_assembly/final.contigs.fa
-bwa mem -t 2 /home/haol7814/ga2018/analyses/02_assembly/final.contigs.fa /home/haol7814/ga2018/data/trimmed_data/SRR4342129_1.paired.trimmed.fastq.gz /home/haol7814/ga2018/data/trimmed_data/SRR4342129_2.paired.trimmed.fastq.gz > /home/haol7814/ga2018/analyses/04_dna_mapping/bwa_dna_map_SRR4342129.sam
+bwa mem -t 2 /home/haol7814/ga2018/analyses/02_assembly/final.contigs.fa /home/haol7814/ga2018/data/trimmed_data/SRR4342129_1.paired.trimmed.fastq.gz /home/haol7814/ga2018/data/trimmed_data/SRR4342129_2.paired.trimmed.fastq.gz | samtools sort -O BAM -o /home/haol7814/ga2018/analyses/04_dna_mapping/SRR4342129.bam -
 
